b64cde04833ab54fe247021b44290f9286e474e2
yuanchen <yuanchenshi@gmail.com>
2020-03-29 22:35:59 -0400

    nba

--- nba/nba-web/src/nba-client.js
+++ nba/nba-web/src/nba-client.js
@@ -1,16 +1,16 @@
 import nba from 'nba';
 
-const SERVER_URL = 'https://34.67.117.84';
-// const SERVER_URL = 'http://35.235.84.235:5000';
+//const SERVER_URL = 'https://34.67.117.84';
+const SERVER_URL = 'http://35.235.84.235:5000';
 export default {
-    ...nba,
-    stats: {
-        ...nba.stats,
-        playerInfo: function({ PlayerID }) {
-            return fetch(`${SERVER_URL}/players/${PlayerID}`).then(res => res.json())
-        },
-        shots: function({ PlayerID }) {
-            return fetch(`${SERVER_URL}/players/${PlayerID}/shots`).then(res => res.json())
-        },
-    },
+ ...nba,
+ stats: {
+   ...nba.stats,
+   playerInfo: function({ PlayerID }) {
+     return fetch(`${SERVER_URL}/players/${PlayerID}`).then(res => res.json())
+   },
+   shots: function({ PlayerID }) {
+     return fetch(`${SERVER_URL}/players/${PlayerID}/shots`).then(res => res.json())
+   },
+ },
 };
